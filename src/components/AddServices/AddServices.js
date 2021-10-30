c5b7771e30839f566a143288065a67b49d9732e4
Mahfuz-Zaman <zaman.13121996@.com>
2021-10-31 02:26:05 +0600

    img add

--- src/components/AddServices/AddServices.js
+++ src/components/AddServices/AddServices.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 import useAuth from '../../hooks/useAuth';
+import addService from "./../../images/addService.jpg"
 
 
 const AddServices = () => {
@@ -26,24 +28,33 @@ const AddServices = () => {
 
     return (
         <div>
+            <Row>
+                <Col>
+                    <div>
 
-            <h2 className='custom-h1 text-center bg-warning mb-0'>Choose To Add Your Expected Event</h2>
-            <div className='text-center bg-dark mt-0'>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <input required className='p-3 m-2' type='text' placeholder='Title' {...register("title")} />
-                    <br />
-                    <input required className='p-3 m-2' type='text' placeholder='Image-url' {...register("image")} />
-                    <br />
-                    <input required className='p-3 m-2' type='text' placeholder='Description' {...register("desc")} />
-                    <br />
-                    <input required className='p-3 m-2' type='number' placeholder='Price' {...register("price")} />
-                    <br />
-                    {errors.exampleRequired && <span>This field is required</span>}
-                    <br />
-                    <input className='btn btn-success ms-2 my-5' value='Include Service' type="submit" />
-                </form>
-            </div>
+                        <h2 className='custom-h1 text-center bg-warning mb-0'>Choose To Add Your Expected Event</h2>
+                        <div className='text-center bg-dark mt-0'>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <input required className='p-3 m-2' type='text' placeholder='Title' {...register("title")} />
+                                <br />
+                                <input required className='p-3 m-2' type='text' placeholder='Image-url' {...register("image")} />
+                                <br />
+                                <input required className='p-3 m-2' type='text' placeholder='Description' {...register("desc")} />
+                                <br />
+                                <input required className='p-3 m-2' type='number' placeholder='Price' {...register("price")} />
+                                <br />
+                                {errors.exampleRequired && <span>This field is required</span>}
+                                <br />
+                                <input className='btn btn-success ms-2 my-5' value='Include Service' type="submit" />
+                            </form>
+                        </div>
+                    </div></Col>
+                <Col className="bg-dark" >
+                    <img className="mt-5" src={addService} alt="" />
+                </Col>
+            </Row>
         </div>
+
     );
 };
 
